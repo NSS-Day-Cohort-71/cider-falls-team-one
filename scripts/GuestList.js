dca927b23132e3e4bc4a4d135aef71203b6c8bae
Lane Dillard <lane.holland04@gmail.com>
2024-06-14 10:36:44 -0500

    guestList_algorith

---
+++ scripts/GuestList.js
@@ -0,0 +1,31 @@
+//import { getGuests } from "./database.js"
+//?import { getLocations } from "./database.js"
+
+//? locations = getLocations()
+
+//guests = getGuests()
+
+
+//Meant to iterate through to make the guest list that will display on the right side of the website.
+// export const guestList = () => {
+    // let guestHTML = "<ul>"
+    
+    // for (const guest of guests) {
+    //     guestHTML += `<li>${guest.name}</li>`
+    // }
+    // guestHTML += "</ul>"
+
+    // return guestHTML
+// }
+
+
+//?Potentially used for the pairing of guest to there location maybe
+//? export const guestPairings() {
+    
+//? }
+
+//? Maybe potentially do pairings of guests to there wanted service
+//? export const guestServices() {
+
+//? }
+
