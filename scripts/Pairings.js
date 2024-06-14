228fb6bcdf3a3d96ca533c2b2881ca7c1d90c17b
Cody Clark <codyjamesclark@gmail.com>
2024-06-14 14:07:39 -0500

    added initial code for pairings module

--- scripts/Pairings.js
+++ scripts/Pairings.js
@@ -7,12 +7,24 @@
 //declare a function that finds what services a location provides
 
 //function should take three parameters? (location object, locationservicesarray, servicesarray)
-
-//declare variable match = null
-//declare empty array locationservices = []
-
+export const getLocationServices = (locationObject, locationServicesArray, servicesArray) => {
+    //declare variable match = null
+    let match = null
+    //declare empty array locationservices = []
+    let matchedServices = []
 //for the location object, if the location id matches the pairing location id, pairobject = match
+    for ( const locationService of locationServicesArray) {
+        if (locationObject.id === locationService.locationId) {
+            match = service
+        }
+        //for the pair object, if pairobject.id = service.id push service.name to locationservices
+        for ( const service of servicesArray) {
+            if ( match.serviceId === service.id ) {
+                matchedServices.push(service.name)
+            }
+        }
+    }
+    //return locationservices
+    return matchedServices
+}
 
-//for the pair object, if pairobject.id = service.id push service.name to locationservices
-
-//return locationservices
