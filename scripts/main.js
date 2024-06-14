c1e9005427a29dd16546dbce04be051077b14fd4
Cody Clark <codyjamesclark@gmail.com>
2024-06-14 11:35:57 -0500

    added comments for logic of main algorithm

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+//import services()
+//import locations()
+//import guests()
+
+//get container from index.html and save to a variable using document.query selector
+
+//set a base html string to be added to the dom later ie mainHTML
+
+//<h1>Cider Falls</h1> //logo here? 
+//<section>services()</section>
+//<section>locations()</section>
+//<section>guests()</section>
+//<footer>Phone Number, Address, Email</footer>
+
+//container.innerHTML = mainHTML
