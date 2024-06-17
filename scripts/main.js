4f82809c067d9b6caa761711e49cd2ed6c0ee6db
Lane Dillard <lane.holland04@gmail.com>
2024-06-17 10:09:05 -0500

    working main with guestList

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,28 @@
 //import services()
 //import locations()
-//import guests()
+import { guestList } from "./GuestList.js"
 
 
 //get container from index.html and save to a variable using document.query selector
+const container = document.querySelector("#container")
+
 
 //set a base html string to be added to the dom later ie mainHTML
+
+const mainHTML = `
+<article class="details">
+    <section class="guests">
+        ${guestList()}
+    </section>
+    <section class="locationService">
+    
+    </section>
+    <section class="location">
+    
+    </section>
+    
+</article>
+`
 
 //<h1>Cider Falls</h1> //logo here? 
 //<section>services()</section>
@@ -13,4 +30,4 @@
 //<section>guests()</section>
 //<footer>Phone Number, Address, Email</footer>
 
-//container.innerHTML = mainHTML
+container.innerHTML = mainHTML
